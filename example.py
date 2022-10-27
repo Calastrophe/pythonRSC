31363d3c734965daf92c56b95ead19bd46969f0e
Calastrophe <74928681+Calastrophe@users.noreply.github.com>
2022-10-27 16:47:02 -0500

    assembler working, reworked all instructions

--- example.py
+++ example.py
@@ -2,6 +2,4 @@ from pyRSC import RSC
 
 if __name__ == "__main__":
     pyRSC = RSC("tests\\microcode\\testing.txt")
-    print(pyRSC._instructions)
     pyRSC.run() # Runs the given instructions and gives you an output!
-    print(pyRSC._memory_layout)
--- pyRSC_assembler.py
+++ pyRSC_assembler.py
@@ -7,12 +7,11 @@ class Assembler():
     def __init__(self, fn, output=None):
         self.fn = fn
         self.output = output if output is not None else fn
-        self.instructions = []
-        self._index = 0
-        self.symbol_table = {}
-        self._match_list = []
         self.memory_layout = {}
-        self.regex_dict = { ## These are not very good regex expressions, but they get the job done. Feel free to change.
+        self._instructions = []
+        self._symbol_table = {}
+        self._matches = []
+        self._regex_dict = { ## These are not very good regex expressions, but they get the job done. Feel free to change.
             InstructionSet.HALT.value : "HALT",
             InstructionSet.LDAC.value : "LDAC ([^\s]+)",
             InstructionSet.STAC.value: "STAC ([^\s]+)",
@@ -33,9 +32,10 @@ class Assembler():
             "LABEL": "([^\s]+):"
         }
         self.obtain_matches()
-        self.scaffold()
+        self.parse_symbols()
+        self.construct_instructions()
         self.replace_symbols()
-        self.create_memory()
+        self.construct_mem()
 
     def obtain_matches(self):
         with open(self.fn, "r") as file:
@@ -43,56 +43,69 @@ class Assembler():
                 if line != '\n':
                     match = self.match_line(line.strip())
                     if match is not None:
-                        self._match_list.append(match[0])
+                        self._matches.append(match[0])
 
-    def scaffold(self):
-        for count, _match in enumerate(self._match_list):
-            match _match[0]:
+    ## Parse the symbols and construct indices for those symbols that will end up in the instructions.
+    ## JMP,JMPZ,LDAC,STAC are all "8-byte wide", essentially meaning they take up two instructions to perform, so +2 index.
+    def parse_symbols(self):
+        index = 0
+        for match in self._matches:
+            match match[0]:
                 case "VARIABLE_ASSIGNMENT":
-                    split_match = _match[1].split(":")
-                    self.instructions.append(split_match[1].strip())
-                    self.symbol_table.update({split_match[0].strip(): count+1})
+                    split_match = match[1].split(":")
+                    self._symbol_table.update({index: split_match[1].strip()})
+                    self._symbol_table.update({split_match[0]: index})
+                    index += 1
                     pass
                 case "LABEL":
-                    split_match = _match[1].split(":")
-                    self.symbol_table.update({split_match[0].strip(): count+1})
+                    split_match = match[1].split(":")
+                    self._symbol_table.update({split_match[0]: index})
                     pass
                 case InstructionSet.JMP.value | InstructionSet.JMPZ.value | InstructionSet.LDAC.value | InstructionSet.STAC.value:
-                    split_match = _match[1].split(" ")
-                    self.instructions.append(_match[0])
-                    self.instructions.append(split_match[1].strip())
+                    index += 2
                     pass
                 case _:
-                    self.instructions.append(_match[0])
+                    index += 1
                     pass
-    
+
+    # Scaffold out the instructions with labels and variables still not changed.
+    def construct_instructions(self):
+        for match in self._matches:
+            match match[0]:
+                case "LABEL":
+                    pass
+                case InstructionSet.JMP.value | InstructionSet.JMPZ.value | InstructionSet.LDAC.value | InstructionSet.STAC.value:
+                    self._instructions.extend([match[0], match[1].split(" ")[1]]) ## We need to add the operand as the next instruction
+                case _:
+                    self._instructions.append(match[0])
+
+    # Replace the stored indices with their values and replace symbols with their respective indices
     def replace_symbols(self):
-        for count, instr in enumerate(self.instructions):
-            if instr in self.symbol_table.keys():
-                self.instructions[count] = hex(self.symbol_table[instr])
-        return
-    
-    def create_memory(self):
-        for count, instr in enumerate(self.instructions):
-            self.memory_layout.update({count: instr})
+        for count, instruction in enumerate(self._instructions): # Attach a number to each instruction
+            if count in self._symbol_table: # If that number is stored in our symbol table, replace that instruction with the stored one.
+                self._instructions[count] = hex(int(self._symbol_table[count], base=16))
+            elif instruction in self._symbol_table:
+                self._instructions[count] = hex(self._symbol_table[instruction])
 
 
+    # Take the instructions and lay them out in memory form to be modified by ldac, stac opcodes.
+    def construct_mem(self):
+        for count, instruction in enumerate(self._instructions):
+            self.memory_layout.update({count: instruction})
+
+    # Dirty function to output in the binary format for Logisim
+    def logisim_format(self, fn):
+        with open(fn, "w") as file:
+            for instruction in self._instructions:
+                file.write((bin(int(instruction, base=16))[2:].zfill(32)) + "\n")
 
     def match_line(self, line) -> List[tuple]:
-        # re.match( "|".join( self.dict_items() ), line)    This is extremely useful but not exactly easy to use and we don't call .match() a bunch of times.
         matches : List[re.Match] = []
-        for item in self.regex_dict.items():
+        for item in self._regex_dict.items():
             regex_str = item[1]
             out = re.match(regex_str, line)
             if out is not None:
                 matches.append((item[0],out.group(0)))
         return matches if len(matches) > 0 else None
-
-
-
-if __name__ == "__main__":
-    tester_obj = Assembler("tests\\microcode\\tester.txt")
-    print(tester_obj.instructions)
-    print(tester_obj.memory_layout)
     
     
